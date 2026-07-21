a7f202f965c5d49866f7a7b34bccb93cf58afa2a
jyostnak <jyostnakondepi@gmail.com>
2026-07-21 22:23:49 +0530

    Creating manager file and outlining the add_password function

---
+++ manager.py
@@ -0,0 +1,20 @@
+def add_password():
+
+    # Take the input from user
+    website = input("Website: ")
+    username = input("Username: ")
+    password = input("Password: ")
+
+    # Store the info in the json file
+    
+
+
+
+def view_passwords():
+    ...
+
+def search_password():
+    ...
+
+def generate_password():
+    ...
