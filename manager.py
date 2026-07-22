c9ec1934a0b0ddec07af01927cdc263161c72e17
jyostnak <jyostnakondepi@gmail.com>
2026-07-22 13:44:44 +0530

    Updating the add_password function.

--- manager.py
+++ manager.py
@@ -1,11 +1,23 @@
-def add_password():
+import storage
+
+def add_password():        # Adds password and it's related details into the JSON file
 
     # Take the input from user
     website = input("Website: ")
     username = input("Username: ")
     password = input("Password: ")
 
-    # Store the info in the json file
+    # Load the data
+    data = storage.load_data()
+
+    # Update the data
+    data[website] = {
+        "username" : username,
+        "password" : password
+    }
+
+    # Add the data back to the file
+    storage.save_data(data)
     
 
 
